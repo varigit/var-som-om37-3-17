03255d803fd9776b4e34e9a81ccc9f9d1178a7b3
Leonid Segal <leonids@variscite.com>
2016-12-20 15:09:55 +0200

    Removing blinking cursor.

--- drivers/tty/vt/vt.c
+++ drivers/tty/vt/vt.c
@@ -1687,7 +1687,7 @@ static void reset_terminal(struct vc_data *vc, int do_clear)
 	vc->vc_decscnm		= 0;
 	vc->vc_decom		= 0;
 	vc->vc_decawm		= 1;
-	vc->vc_deccm		= global_cursor_default;
+	vc->vc_deccm		= 0;
 	vc->vc_decim		= 0;
 
 	vt_reset_keyboard(vc->vc_num);
